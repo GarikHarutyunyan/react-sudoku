1b877c13b20a93778aa73d903e4ab5b70762fd75
Garik <garik.harutyunian@gmail.com>
2023-04-01 21:06:12 +0400

    4/Create Cell and Matrix Components

---
+++ src/pages/game/Game.tsx
@@ -0,0 +1,12 @@
+import { Matrix } from "./components/matrix/Matrix";
+import "./game.css";
+
+const Game = () => {
+  return (
+    <div className={"game"}>
+      <Matrix />
+    </div>
+  );
+};
+
+export { Game };
