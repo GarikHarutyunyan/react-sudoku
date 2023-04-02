823ac3e3aa27b7298449ded124330146137184a0
Garik <garik.harutyunian@gmail.com>
2023-04-02 22:16:05 +0400

    11/Use react router

--- src/pages/level/Level.tsx
+++ src/pages/level/Level.tsx
@@ -1,21 +1,26 @@
 import {useEffect} from 'react';
 import {Matrix} from './components/matrix/Matrix';
-import './game.css';
+import './level.css';
 import {useDispatch, useSelector} from 'react-redux';
 import {checkIsSolved, getLevel} from '../../store/levelSlice';
 import {NumberBoard} from './components/number-board/NumberBoard';
 import {Win} from './components/win/Win';
+import {Link, useParams} from 'react-router-dom';
 
-const Game = () => {
+const Level = () => {
   const dispatch = useDispatch<any>();
   const isSolved = useSelector(checkIsSolved);
+  const {id} = useParams();
 
   useEffect(() => {
-    dispatch(getLevel('b'));
+    dispatch(getLevel(id as string));
   }, []);
 
   return (
-    <div className={'game'}>
+    <div className={'level'}>
+      <h3>
+        <Link to={'/'}>{'Back'}</Link>
+      </h3>
       {isSolved ? (
         <Win />
       ) : (
@@ -28,4 +33,4 @@ const Game = () => {
   );
 };
 
-export {Game};
+export {Level};
